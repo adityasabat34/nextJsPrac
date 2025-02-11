b749cfdf1a29b2e732b882376ca8e313c40267f2
adityasabat34 <147155098+adityasabat34@users.noreply.github.com>
2025-02-11 21:53:16 +0530

    Navigation learnt using Link tag in NEXTjs and also replace attribute for redirect initial page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <h1>Welcome home</h1>;
+  return (
+    <>
+      <h1>Welcome to NextJS</h1>
+      <Link href="/blog">Blog navigate</Link>
+      <Link href="/products">navigate to product page</Link>
+    </>
+  );
 }
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,12 +1,19 @@
-export default function Products() {
+import Link from "next/link";
+
+export default function Products({ productId = 100 }) {
   return (
     <>
-      <h1>All products list</h1>
-      <h2>Product 1</h2>
+      <Link href={"/products"}>
+        <h1>All products list</h1>
+      </Link>
+      <Link href={`products/${productId}`} replace>
+        <h2>Product 1</h2>
+      </Link>
       <h2>Product 2</h2>
       <h2>Product 3</h2>
       <h2>Product 4</h2>
       <h2>Product 5</h2>
+      <Link href={"/"}>Home</Link>
     </>
   );
 }
