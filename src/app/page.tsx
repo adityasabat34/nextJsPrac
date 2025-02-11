b9ea6f42e28c21b587fc2a484b33c70573880270
adityasabat34 <147155098+adityasabat34@users.noreply.github.com>
2025-02-11 22:07:06 +0530

    Dynamic navigation routing in client side

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
-import Link from "next/link";
+"use client";
 
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 export default function Home() {
+  const router = useRouter();
+
   return (
     <>
       <h1>Welcome to NextJS</h1>
       <Link href="/blog">Blog navigate</Link>
       <Link href="/products">navigate to product page</Link>
+
+      <button onClick={() => router.push("/products")}>Place Order </button>
     </>
   );
 }
