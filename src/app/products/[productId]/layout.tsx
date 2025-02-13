f789818831ec3c8c97f5ee3a03ab9c17c9dfffde
adityasabat34 <147155098+adityasabat34@users.noreply.github.com>
2025-02-13 21:56:20 +0530

    dynamic metadata and multiple root layouut

---
+++ src/app/products/[productId]/layout.tsx
@@ -0,0 +1,12 @@
+const ComponentName = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      {children}
+      <h1>Featured Products</h1>
+    </>
+  );
+};
+
+export default ComponentName;
+
+// first this layout is replace with root layout file so first root layout will render then it will check whether their layout page exist or not if exist then it will over write or ignored(root layout)
--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,3 +1,23 @@
+import { Metadata } from "next";
+type Props = {
+  params: Promise<{ productId: string }>;
+};
+
+export const generateMetadata = async ({
+  params,
+}: Props): Promise<Metadata> => {
+  const id = (await params).productId;
+  const title = await new Promise((res) => {
+    setTimeout(() => {
+      res(`iPhone ${id}`);
+    }, 100);
+  });
+
+  return {
+    title: `Product ${title}`,
+  };
+};
+
 export default async function ProductList({
   params,
 }: {
