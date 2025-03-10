e4e62d6b471c4f8c44bb5480e776fa6d8472d10a
adityasabat34 <147155098+adityasabat34@users.noreply.github.com>
2025-03-10 23:59:24 +0530

    working on template in next js

---
+++ src/app/(auth)/page.tsx
@@ -0,0 +1,9 @@
+const NavLink = () => {
+  const navLinks = [
+    { name: "Register", href: "/register" },
+    { name: "Login", href: "/login" },
+  ];
+  return <div>ComponentName</div>;
+};
+
+export default NavLink;
