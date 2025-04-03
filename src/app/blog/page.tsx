f721eedbc234f5d1f6ce29d03fe474ac3c84c6b6
adityasabat34 <147155098+adityasabat34@users.noreply.github.com>
2025-04-03 23:38:03 +0530

    Loading in nextjs

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,6 +1,17 @@
 import Link from "next/link";
+import { Metadata } from "next";
 
-export default function Blog() {
+export const metadata: Metadata = {
+  title: {
+    absolute: "blog",
+  },
+};
+export default async function Blog() {
+  await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("loading");
+    }, 2000);
+  });
   return (
     <>
       <h1>hello world</h1>
